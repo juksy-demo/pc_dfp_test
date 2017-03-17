fc7d586278e40615da99ec367d6aa690b004e3c1
Anny <anny@juksy.com>
2017-03-17 17:12:08 +0800

    base

---
+++ index_files/external.php
@@ -0,0 +1,7 @@
+/* out-freq, hit 2 tw */
+/* silent, no pid */
+if (ONEAD_get_response[0] !== undefined && typeof ONEAD_get_response[0] == 'function'){
+    ONEAD_get_response[0](null);
+}
+/* server */
+
